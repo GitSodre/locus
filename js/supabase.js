0b3de987884b47f50a04818f23fc9eb796559547
GitSodre <sodrelucas221330@gmail.com>
2026-03-25 18:44:44 -0300

    Change Supabase ANON_KEY to a new value
    Updated the Supabase anonymous key for the client.

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // js/supabase.js
 const SUPABASE_URL = "https://tkqdqydcthdztitfzmrs.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI";
+const SUPABASE_ANON_KEY = "sb_publishable_i4isIUlDwBdLYQBcmEDijw_lFJmKhNG";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
