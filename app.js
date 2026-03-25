170922222e178a283be2b0334249d7c99d29a6bf
GitSodre <sodrelucas221330@gmail.com>
2026-03-24 21:52:32 -0300

    Refactor login function with improved error handling
    Updated error handling in the login function to display the error message.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const supabaseUrl = https://tkqdqydcthdztitfzmrs.supabase.co;
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrcWRxeWRjdGhkenRpdGZ6bXJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMTQxMjQsImV4cCI6MjA4OTg5MDEyNH0.ZWv6OvU3l-9qHHUu--9dyIaOxl672tm59u8I_qhLTzo;
+const supabaseUrl = "https://tkqdqydcthdztitfzmrs.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrcWRxeWRjdGhkenRpdGZ6bXJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMTQxMjQsImV4cCI6MjA4OTg5MDEyNH0.ZWv6OvU3l-9qHHUu--9dyIaOxl672tm59u8I_qhLTzo";
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
 async function logar() {
   const login = document.getElementById("login").value;
   const senha = document.getElementById("senha").value;
@@ -14,7 +15,7 @@ async function logar() {
   });
 
   if (error) {
-    alert("Erro no login");
+    alert("Erro no login: " + error.message);
   } else {
     alert("Login realizado!");
     window.location.href = "dashboard.html";
